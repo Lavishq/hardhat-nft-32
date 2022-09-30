d6299f1f81fb8ee47832d8df8457bc74976a0441
Lavishq <v.whitehearted@simplelogin.co>
2022-09-30 19:41:09 +0530

    wrote tests

--- deploy/02-deploy-random-ipfs.js
+++ deploy/02-deploy-random-ipfs.js
@@ -1,16 +1,30 @@
 const { network, ethers } = require("hardhat")
 const { developmentChains, networkConfig } = require("../helper-hardhat-config")
 const { verify } = require("../utils/verify")
-const { storeImages } = require("../utils/uploadToPinata")
+const { storeImages, storeTokenUriMetadata } = require("../utils/uploadToPinata")
 
 const imagesLocation = "./images/randomNft"
+
+const metadataTemplate = {
+    name: "",
+    description: "",
+    image: "",
+    attributes: [{ trait_types: "Cuteness", value: 33 }],
+}
+
+let tokenUris = [
+    "ipfs://QmZNaHxCsQ1GRtaUALRzifTnCkZ6dbRMmaUaj8GkmZGauH",
+    "ipfs://QmQufxophrgr22q3xihKQnGQ2v2UpBVmdXKSymFdtZ7mo3",
+    "ipfs://QmRPCb884rLXthgqTmCoMHmnaqD93DiJt5H5WPkLYbLFBB",
+]
+
+const FUND_AMOUNT = ethers.utils.parseEther("10") // 10 link
 
 module.exports = async function ({ getNamedAccounts, deployments }) {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
 
     const chainId = network.config.chainId
-    let tokenUris
 
     if (process.env.UPLOAD_TO_PINATA == "true") {
         tokenUris = await handleTokenUris()
@@ -23,26 +37,48 @@ module.exports = async function ({ getNamedAccounts, deployments }) {
         const tx = await vrfCoordinatorV2Mock.createSubscription()
         const txReceipt = await tx.wait(1)
         subscriptionId = txReceipt.events[0].args.subscriptionId
+        await vrfCoordinatorV2Address.fundSubscription(subscriptionId, FUND_AMOUNT)
     } else {
         vrfCoordinatorV2Address = networkConfig[chainId].vrfCoordinatorV2
         subscriptionId = networkConfig[chainId].subscriptionId
     }
 
     log("--------------------------")
-    await storeImages(imagesLocation)
-    // const args = [
-    //     vrfCoordinatorV2Address,
-    //     subscriptionId,
-    //     networkConfig[chainId].gasLane,
-    //     networkConfig[chainId].callbackGasLimit,
-    //     //tokenuri,
-    //     networkConfig[chainId].mintFee,
-    // ]
+    // await storeImages(imagesLocation) // commented as already stored proframmatically
+    const args = [
+        vrfCoordinatorV2Address,
+        subscriptionId,
+        networkConfig[chainId].gasLane,
+        networkConfig[chainId].callbackGasLimit,
+        tokenUris,
+        networkConfig[chainId].mintFee,
+    ]
+    const randomIpfsNft = await deploy("RandomIpfsNft", {
+        from: deployer,
+        args: args,
+        log: true,
+        waitConfirmations: network.config.blockConfirmations || 1,
+    })
+    log("--------------------------")
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        log("Verfying...")
+        await verify(randomIpfsNft.address, args)
+    }
 }
 
 async function handleTokenUris() {
-    tokenUris = []
-    return tokenUris
+    let tokenUris
+    const { responses: imageUploadResponses, files } = await storeImages(imagesLocation)
+    for (imageUploadResponseIndex in imageUploadResponses) {
+        let tokenUriMetadata = { ...metadataTemplate }
+        tokenUriMetadata.name = files[imageUploadResponseIndex].replace(".png", "")
+        tokenUriMetadata.description = `An adorable ${tokenUriMetadata.name} ~~~`
+        tokenUriMetadata.image = `ipfs://${imageUploadResponses[imageUploadResponseIndex].IpfsHash}`
+        console.log(`uploading ${tokenUriMetadata.name}...`)
+        const metadataUploadResponse = await storeTokenUriMetadata(tokenUriMetadata)
+        tokenUris.push(`ipfs://${metadataUploadResponse.IpfsHash}`)
+    }
+    console.log("token uris uploaded! They are:")
+    console.log(tokenUris)
 }
-
-module.exports.tags = ["all", "randomipfs", "main", "custom"]
+module.exports.tags = ["all", "randomIpfs", "main", "custom"]
--- utils/uploadToPinata.js
+++ utils/uploadToPinata.js
@@ -1,11 +1,39 @@
 const pinataSDK = require("@pinata/sdk")
 const path = require("path")
 const fs = require("fs")
+require("dotenv").config()
 
-async function storeImage(imagesFilePath) {
+const pinataApiKey = process.env.PINATA_API_KEY
+const pinataApiSecret = process.env.PINATA_API_SECRET
+const pinata = pinataSDK(pinataApiKey, pinataApiSecret)
+
+async function storeImages(imagesFilePath) {
     const fullImagePath = path.resolve(imagesFilePath)
     const files = fs.readdirSync(fullImagePath)
 
-    console.log(files)
+    let responses = []
+    console.log("uploading to pintata")
+    for (fileIndex in files) {
+        console.log("file", fileIndex)
+        const readableStreamForFile = fs.createReadStream(`${fullImagePath}/${files[fileIndex]}`)
+        try {
+            const reponse = await pinata.pinFileToIPFS(readableStreamForFile)
+            responses.push(reponse)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    return { responses, files }
 }
-module.exports = { storeImage }
+
+async function storeTokenUriMetadata(metadata) {
+    try {
+        const reponse = await pinata.pinJSONToIPFS(metadata)
+        return reponse
+    } catch (error) {
+        console.log(error)
+    }
+    return null
+}
+
+module.exports = { storeImages, storeTokenUriMetadata }
